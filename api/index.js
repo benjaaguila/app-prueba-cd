803552b99121804a774cb905b191e18c5123cc62
Benjamin Aguila <correo@foo.com>
2024-04-07 12:17:26 -0400

    feat GEt /v4

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 // GET endpoint
 app.get('/', (req, res) => {
-  res.send('Hello, this is a GET request! v3');
+  res.send('Hello, this is a GET request! v4');
 });
 
 app.get('/healthcheck', (req, res) => {
